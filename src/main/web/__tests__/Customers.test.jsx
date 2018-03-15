cf4068b630d5383b176381a0a2a8613221fc3057
Didier Parat <didier.parat@telenordigital.com>
2018-03-15 20:46:08 +0100

    Fix unit tests

--- src/main/web/__tests__/Customers.test.jsx
+++ src/main/web/__tests__/Customers.test.jsx
@@ -15,21 +15,21 @@ describe('Test <Customers> with wrapping in <Provider>',()=>{
     it('+++ renders correctly', () => {
         const setData = (data)=>{};
         const rendered = renderer.create(
-            <Customers apiUrl={'http://localhost:8080'} data={{}} setData={setData} />
+            <Customers apiUrl={'http://localhost:8080/'} data={{}} setData={setData} />
         );
         expect(rendered.toJSON()).toMatchSnapshot();
     });
 
     it('+++ clicking button sets new data', () => {
         const setData = (data)=>{};
-        const wrapper = shallow( <Customers apiUrl={'http://localhost:8080'} data={{}} setData={setData} /> );
+        const wrapper = shallow( <Customers apiUrl={'http://localhost:8080/'} data={{}} setData={setData} /> );
         wrapper.find('button').simulate('click');
-        expect(fetchAndSetData).toBeCalledWith('http://localhost:8080/customers/123', setData);
+        expect(fetchAndSetData).toBeCalledWith('http://localhost:8080/customers/12345678901', setData);
     });
 
     it('+++ data is displayed as json', () => {
         const setData = (data)=>{};
-        const wrapper = shallow( <Customers apiUrl={'http://localhost:8080'} data={{status: 200}} setData={setData} /> );
+        const wrapper = shallow( <Customers apiUrl={'http://localhost:8080/'} data={{status: 200}} setData={setData} /> );
         expect(wrapper.find('pre').get(0).props.children).toBe("{\n  \"status\": 200\n}");
     });
 });
